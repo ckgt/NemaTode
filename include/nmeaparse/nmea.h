09a3c9c3485a023b5ae98652c46129941ca7fd41
ckgt <ckarlsson25@gmail.com>
2015-03-28 19:08:43 -0400

    Initial commit.

---
+++ include/nmeaparse/nmea.h
@@ -0,0 +1,28 @@
+/*
+* nmea.h
+*
+*  Created on: March 23, 2014
+*      Author: Cameron Karlsson
+*/
+
+
+// The implementation of a NMEA 0183 parser.
+// The implementation of a NMEA 0183 sentence generator.
+// The implementation of a GPS data service.
+
+
+#ifndef NMEA_H_
+#define NMEA_H_
+
+
+
+#include <nmeaparse/NMEAParser.h>
+#include <nmeaparse/NMEACommand.h>
+#include <nmeaparse/GPSService.h>
+
+#include <nmeaparse/NumberConversion.h>
+
+
+
+
+#endif NMEA_H_
